1329b43ffeae3364328a7be3578528e7654dc845
bun913 <imaizumi.taiki@classmethod.jp>
2022-11-22 06:36:10 +0900

    feat: add ecr repos

--- lib/step_example-stack.ts
+++ lib/step_example-stack.ts
@@ -1,16 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecr from 'aws-cdk-lib/aws-ecr'
 
 export class StepExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'StepExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const okRepo = new ecr.Repository(this, 'OKRepo')
+    const ngRepo = new ecr.Repository(this, 'NGRepo')
   }
 }
