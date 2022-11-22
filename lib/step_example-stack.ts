d57919537a7360ce93b51487fb41fd0186d1aa33
bun913 <imaizumi.taiki@classmethod.jp>
2022-11-22 15:49:09 +0900

    feat: ECSを配置するためのNetworkリソースを追加

--- lib/step_example-stack.ts
+++ lib/step_example-stack.ts
@@ -1,12 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Ecr } from './resources/ecr'
+import { Network } from './resources/network'
 
 export class StepExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    env: {
+      region: 'ap-northeast-1'
+    }
     // ECR
     const ecr = new Ecr(this)
     ecr.createResources()
+    // Network
+    const network = new Network(this)
+    network.createResources()
   }
 }
