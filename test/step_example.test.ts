8bb714e26f331418096c75f345bd047e9579029d
bun913 <imaizumi.taiki@classmethod.jp>
2022-11-22 09:36:01 +0900

    feat: add snapshot test

--- test/step_example.test.ts
+++ test/step_example.test.ts
@@ -1,17 +1,14 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as StepExample from '../lib/step_example-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as StepExample from '../lib/step_example-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/step_example-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
+test('snapshot test', () => {
+  const app = new cdk.App();
 //     // WHEN
-//   const stack = new StepExample.StepExampleStack(app, 'MyTestStack');
+  const stack = new StepExample.StepExampleStack(app, 'MyTestStack');
 //     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  const template = Template.fromStack(stack);
+  expect(template).toMatchSnapshot();
 });
