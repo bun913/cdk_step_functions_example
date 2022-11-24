204775e689ff4b960fd90629a11219388b6f583a
bun913 <imaizumi.taiki@classmethod.jp>
2022-11-24 12:36:25 +0900

    feta: add ecs cluster

--- lib/resources/ecs.ts
+++ lib/resources/ecs.ts
@@ -1,20 +1,24 @@
 import { FargateTaskDefinition } from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
-import { ContainerImage } from 'aws-cdk-lib/aws-ecs'
+import { ContainerImage, Cluster } from 'aws-cdk-lib/aws-ecs'
+import { Vpc,SecurityGroup } from 'aws-cdk-lib/aws-ec2';
 
 export class Ecs {
 
     private scope: Construct
     private okImage: ContainerImage
     private ngImage: ContainerImage
+    private vpc: Vpc
 
-    constructor(scope: Construct, okImage: ContainerImage, ngImage: ContainerImage) {
+    constructor(scope: Construct, okImage: ContainerImage, ngImage: ContainerImage, vpc: Vpc) {
         this.scope = scope
         this.okImage = okImage
         this.ngImage = ngImage
+        this.vpc = vpc
     }
 
     public createResources() {
+        // タスク定義
         const okTaskDef = new FargateTaskDefinition(this.scope, 'okTaskDef', {
             memoryLimitMiB: 512,
             cpu: 256
@@ -34,6 +38,18 @@ export class Ecs {
             containerName: 'ng',
             privileged: false,
             readonlyRootFilesystem: true
+        })
+        // ECSクラスタ
+        const cluster = new Cluster(this.scope, 'FargateCluster', {
+            vpc: this.vpc,
+            clusterName: 'StepExample',
+            containerInsights: false,
+        });
+        // RunTaskの際に利用するSG
+        const sg = new SecurityGroup(this.scope, 'RunTaskSG', {
+            vpc: this.vpc,
+            securityGroupName: 'stepExampleRuntaskSG',
+            allowAllOutbound: true
         })
     }
 }
